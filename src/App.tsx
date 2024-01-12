f4061ec1c8fc0ce7451f1576d910214ffb003869
faris <mrfaristp@gmail.com>
2024-01-12 23:16:18 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-center text-red-700 font-bold text-5xl uppercase'>This is test</p>
+    </>
+  )
+}
+
+export default App
