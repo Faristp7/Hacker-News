12d1385af0c4a8e8b8b30b3f8441ca7ded77fe88
faris <mrfaristp@gmail.com>
2024-01-13 17:49:00 +0530

    sotry section completed

--- src/components/HomeNews/HomeNews.tsx
+++ src/components/HomeNews/HomeNews.tsx
@@ -1,7 +1,6 @@
 import { tailspin } from 'ldrs'
 import React, { useState } from 'react'
 import { fetchStoryDetails, fetchTopStories } from '../../services/hackerNewsApi'
-import ReactTimeAgo from 'react-time-ago'
 
 tailspin.register()
 
@@ -9,7 +8,7 @@ export default function HomeNews() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [topStories, setTopStories] = useState<any>([])
     const [loading, setLoading] = useState<boolean>(true)
-    const [page , setpages] = useState<number>(20)
+    const [page, setpages] = useState<number>(20)
 
     React.useEffect(() => {
         (async () => {
@@ -23,17 +22,9 @@ export default function HomeNews() {
             setLoading(false)
         })()
     }, [page])
-    console.log(topStories);
-
-
-    function formatUnixTimestamp(timestamp: number) {
-        const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
-        const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return date.toLocaleDateString('en-US', options);
-    }
 
     return (
-        <div className="p-2 mt-10 dark:bg-black">
+        <div className="py-1 mt-10 dark:bg-black">
             {loading ? (
                 <l-tailspin
                     size="30"
@@ -43,24 +34,29 @@ export default function HomeNews() {
                 ></l-tailspin>
             ) : (
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                topStories.map((story: any , index : number) => (
-                    <div className='flex gap-8 justify-between mb-0.5' key={story.id}>
-                        <div className='cursor-pointer overflow-hidden'>
-                            <h2>{story.title}</h2>
-                            <p className='font-thin text-sm dark:text-gray-500'>{story.by} <span>21</span></p>
-                            <p className='truncate font-thin text-sm dark:text-gray-500'>{story.url}</p>
-                        </div>
-                        <div className='dark:bg-gray-900 p-1 px-2 flex flex-col justify-between items-center relative '>
-                            <div className='flex justify-center text-orange-500 text-sm'>
-                                16
-                                <img className='absolute top-1 opacity-30 w-6 h-6' src="https://img.icons8.com/metro/26/b36537/speech-bubble.png" alt="speech-bubble" />
+                topStories.map((story: any, index: number) => (
+                    <React.Fragment key={index}>
+                        <div className='flex gap-8 justify-between mb-0.5  border-orange-600 active:border-l-2' key={index}>
+                            <a className='cursor-pointer overflow-hidden' href={story.url} target='_blank'>
+                                <h2>{story.title}</h2>
+                                <p className='font-thin text-sm dark:text-gray-500'>
+                                    {story.by} <span> 30</span>
+                                </p>
+                                <p className='truncate font-thin text-sm dark:text-gray-500'>{story.url}</p>
+                            </a>
+                            <div className='dark:bg-gray-900 p-1 px-2 flex flex-col justify-between items-center relative '>
+                                <div className='flex justify-center text-orange-500 text-sm'>
+                                    16
+                                    <img className='absolute top-1 opacity-30 w-6 h-6' src="https://img.icons8.com/metro/26/b36537/speech-bubble.png" alt="speech-bubble" />
+                                </div>
+                                <div className='text-sm'>118</div>
                             </div>
-                            <div className='text-sm'>118</div>
                         </div>
-                    </div>
+                        <hr className='opacity-45 border-gray-600' />
+                    </React.Fragment>
                 ))
-                )}
-                <p className='text-center cursor-pointer' onClick={() => setpages(page + 10)}>Load More</p>
+            )}
+            <p className='text-center cursor-pointer' onClick={() => setpages(page + 15)}>Load More</p>
         </div>
     )
 }
--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -1,8 +1,8 @@
 
 export default function SearchBox() {
   return (
-    <div>
-      <div className="bg-orange-600 p-1 relative">
+    <div className="p relative">
+      <div className="bg-orange-500 p-1 absolute w-full top-11 z-20">
         <input type="text" className="w-full dark:bg-gray-800 pl-9 py-1.5 rounded" placeholder="Search"/>
         <img className="w-5 h-5 absolute top-3 left-3" src="https://img.icons8.com/ios/50/FF6600/search--v1.png" alt="search--v1"/>
       </div>
